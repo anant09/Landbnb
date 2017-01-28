23f8966c15c49db2a7d278f0144d95e45d9b8d27
Anant Saxena <contact.anant1@gmail.com>
2017-01-28 15:14:48 +0530

    email

--- step2/views.py
+++ step2/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib import messages
+from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
 from .forms import NameForm
 from .models import Courier
@@ -29,6 +31,12 @@ def about(request):
         print "entered post"
         # if form.is_valid():
         q= Courier(user=request.user, weight=request.POST['weight'],height=request.POST['height'],width=request.POST['width'],length=request.POST['length'],location_from=request.POST['location_from'],location_to=request.POST['location_to'])
+        subject = 'Landbnb order'
+        message = 'Your order has been recorded successfully. Please notify if any of the following details are incorrect or need to be changed:- \nWeight :'+ str(request.POST['weight'])+'\nHeight: '+str(request.POST['height'])+'\nWidth: '+str(request.POST['width'])+'\nLength: '+str(request.POST['length'])+'\nFrom: '+str(request.POST['location_from'])+'\nDestination: '+str(request.POST['location_to'])
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [request.user.email]
+
+        send_mail(subject,message,from_email,to_list,fail_silently = True)
         q.save()
         return HttpResponseRedirect('/step2/request_made/')
         # else:
